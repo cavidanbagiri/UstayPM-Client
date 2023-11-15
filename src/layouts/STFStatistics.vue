bf0699da367ac8328033266248d6133ffd093b16
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-11-15 15:57:29 +0300

    Fixed Table Statistic Design

--- src/layouts/STFStatistics.vue
+++ src/layouts/STFStatistics.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="grid grid-cols-10 gap-3 justify-between sticky bg-white">
+  <div class="grid grid-cols-12 gap-4 justify-between sticky bg-white">
 
     <div class=" col-span-2 flex flex-row justify-between border shadow-md w-full p-3 rounded-md
           hover:shadow-xl hover:cursor-pointer ">
       <div class="flex flex-col justify-between">
-        <span class="text-gray-600 analyz_header">Waiting</span>
+        <span class="text-gray-600 analyz_header">
+          <span class="bg-gray-200 text-gray-500 rounded-full px-2 py-2"><i class="fa-solid fa-clock fa-lg"></i></span> 
+          Waiting
+        </span>
         <span class="bg-gray-200 rounded-md text-gray-800 p-2 analyz_get_data text-center">Get Data</span>
       </div>
       <div class="flex items-center ">
@@ -16,7 +19,10 @@
     <div class=" col-span-2 flex flex-row justify-between border shadow-md w-full p-3 rounded-md
           hover:shadow-xl hover:cursor-pointer ">
       <div class="flex flex-col justify-between">
-        <span class="text-gray-600 analyz_header">Completed</span>
+        <span class="text-gray-600 analyz_header">
+          <span class="bg-red-200 text-red-500 rounded-full px-3 py-2"><i class="fa-regular fa-clipboard fa-lg"></i></span> 
+          Completed
+        </span>
         <span class="bg-red-200 rounded-md text-red-800 p-2 analyz_get_data text-center">Get Data</span>
       </div>
       <div class="flex items-center ">
@@ -29,7 +35,10 @@
     <div class=" col-span-2 flex flex-row justify-between border w-full p-3 rounded-md shadow-md 
           hover:shadow-xl hover:cursor-pointer ">
       <div class="flex flex-col justify-between">
-        <span class="text-gray-600 analyz_header">Processing</span>
+        <span class="text-gray-600 analyz_header">
+          <span class="bg-blue-200 text-blue-500 rounded-full px-2 py-2"><i class="fa-solid fa-person-digging fa-lg"></i></span>
+          Processing
+        </span>
         <span class="bg-blue-200 rounded-md text-blue-800 p-2 analyz_get_data text-center">Get Data</span>
       </div>
       <div class="flex items-center ">
@@ -42,7 +51,10 @@
     <div class=" col-span-2 flex flex-row justify-between border w-full p-3 rounded-md
           shadow-md hover:shadow-xl hover:cursor-pointer ">
       <div class="flex flex-col justify-between">
-        <span class="text-gray-600 analyz_header">Received</span>
+        <span class="text-gray-600 analyz_header">
+          <span class="bg-green-200 text-green-500 rounded-full px-2 py-2"><i class="fa-solid fa-satellite-dish fa-lg"></i></span>
+          Received
+        </span>
         <span class="bg-green-200 rounded-md text-green-800 p-2 analyz_get_data text-center">Get Data</span>
       </div>
       <div class="flex items-center ">
@@ -55,7 +67,10 @@
     <div class=" col-span-2 flex flex-row justify-between border w-full p-3 rounded-md
           shadow-md hover:shadow-xl hover:cursor-pointer ">
       <div class="flex flex-col justify-between">
-        <span class="text-gray-600 analyz_header">Warehouse</span>
+        <span class="text-gray-600 analyz_header">
+          <span class="bg-pink-200 text-pink-500 rounded-full px-2 py-2"><i class="fa-solid fa-warehouse fa-lg"></i></span>
+          Warehouse
+        </span>
         <span class="bg-pink-200 rounded-md text-pink-800 p-2 analyz_get_data text-center">Get Data</span>
       </div>
       <div class="flex items-center ">
@@ -65,6 +80,21 @@
       </div>
     </div>
 
+    <div class=" col-span-2 flex flex-row justify-between border shadow-md w-full p-3 rounded-md
+          hover:shadow-xl hover:cursor-pointer ">
+      <div class="flex flex-col justify-between">
+        <span class="text-gray-600 analyz_header">
+          <span class="bg-orange-200 text-orange-500 rounded-full px-2 py-2"><i class="fa-solid fa-ban fa-lg"></i></span> 
+          Canceled
+        </span>
+        <span class="bg-orange-200 rounded-md text-orange-800 p-2 analyz_get_data text-center">Get Data</span>
+      </div>
+      <div class="flex items-center ">
+        <div class="rounded-full flex border-8 border-orange-600 w-16 h-16  items-center justify-center analyz_show_data">
+          12
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
