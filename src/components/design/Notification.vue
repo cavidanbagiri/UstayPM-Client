5a5d84f999752351ea7991ce0550d9dd9f4cd001
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-11-23 14:10:12 +0300

    Create Production mode

---
+++ src/components/design/Notification.vue
@@ -0,0 +1,51 @@
+<template>
+    <div style="font-family: 'Poppins';"
+        class="flex flex-col text-xl absolute top-0 left-12 w-[500px] h-[650px] overflow-auto bg-white py-2 px-3 border rounded-lg shadow-2xl">
+        <div class="flex justify-between">
+            <div class="flex">
+                <span>
+                    <img src="../../assets/bellicon.webp" class="w-10 h-10" alt="">
+                </span>
+                <span class="text-center text-3xl pt-1 pb-2">
+                    Notification
+                </span>
+            </div>
+            <div>
+                <span class="hover:cursor-pointer hover:text-gray-500">
+                    <i class="fa-solid fa-xmark"></i>
+                </span>
+            </div>
+        </div>
+        <ul class="text-sm" style="font-family: 'Roboto';">
+            <li v-for="i in 10" class="border p-1 flex my-2 rounded-lg hover:cursor-pointer hover:bg-gray-100">
+                <!-- User Image -->
+                <div class="flex">
+                    <img class="w-14 h-14 rounded-full"
+                        src="https://static.wixstatic.com/media/033319_e0f92699ac1741baa4b84b6755734bb6~mv2.png/v1/crop/x_89,y_0,w_2601,h_1732/fill/w_480,h_486,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_0297_edited.png"
+                        alt="">
+                    <div class="flex flex-col pl-2 justify-center">
+                        <span class="text-lg font-bold">
+                            Bagirli Cavidan
+                            <span class="font-thin text-sm" style="font-family: 'Poppins';">
+                                <span class="text-red-500">&#9679</span>
+                                Create New STF SRU.RS.07.10445
+                            </span>
+                        </span>
+                        <div class=" flex items-center">
+                            <span class="text-black" style="font-family: 'Poppins';">
+                                06-12-2023 14:45
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/layouts/NavbarLayout.vue
+++ src/layouts/NavbarLayout.vue
@@ -39,9 +39,9 @@
           </router-link>
       </div>
 
-      <div class="relative mt-4" @click="showNotification">
+      <div class="relative mt-4">
             <div class="badge bg-red-500 border-none text-white badge-md absolute top-0 right-0 w-5 h-5 font-bold p-[11px]">{{ notification_data.new_stf_notification }}</div>
-            <span @mouseover="notification_tooltip = true" @mouseleave="notification_tooltip = false"
+            <span @click="showNotification" @mouseover="notification_tooltip = true" @mouseleave="notification_tooltip = false"
                 class="rounded-md cursor-pointer mt-[11px] w-10 h-10 flex flex-row justify-center items-center hover:bg-green-500 duration-300">
                 <i class="fa-solid fa-bell fa-md " style="color:white"></i>
             </span>
@@ -49,6 +49,7 @@
                 class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 py-2 px-4 text-white text-md rounded-md flex">
                 Notification
             </span>
+            <Notification v-if="notification_toggle" />
       </div>
       <div class="relative">
           <router-link to="/">
@@ -127,8 +128,6 @@
           </router-link>
       </div>
 
-      
-
       <div v-if="!user_store.user" class="relative">
           <router-link to="/user/login">
               <span @mouseover="login_tooltip = true" @mouseleave="login_tooltip = false"
@@ -161,6 +160,7 @@
 <script setup>
 
 import { ref, watchEffect, reactive } from 'vue';
+import Notification from '../components/design/Notification.vue';
 import UserStore from '../store/store.user_store';
 import IndexStore from '../store/store.index';
 
@@ -186,6 +186,7 @@ const logout_tooltip = ref(false);
 
 // Get User Inform
 const user = ref();
+const notification_toggle = ref(false);
 
 // Get User Notification Inform
 const notification_data = reactive({
@@ -203,6 +204,7 @@ watchEffect(() => {
 
 const showNotification = () => {
     if(user_store.user){
+        notification_toggle.value = !notification_toggle.value 
         index_store.readNotification(user_store?.user?.id)
         notification_data.new_stf_notification = 0;
     }
