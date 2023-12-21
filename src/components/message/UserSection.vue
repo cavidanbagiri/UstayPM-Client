04ca6f61fa3cc31c4943b7500882749d0eebfb12
cavidanbagiri <cavidanbagiri@gmail.com>
2023-12-22 00:29:57 +0300

    Solve Another page and coming user message notification

--- src/components/message/UserSection.vue
+++ src/components/message/UserSection.vue
@@ -122,28 +122,28 @@ const selectedUser = async (user) => {
     }
 }
 
-watchEffect(()=>{
-    socket.on('broadcastunreadcountingmessages', data => {
-        if(data[data.length - 1].senderId == user_store.user?.id){
-            // Iterate All User and find it
-            for(let user of message_store.unread_messages_and_users){
-                if(user.id == data[data.length-1].receiverId ){
-                    // Replace finding user with sending message user
-                    user.roomid = data[0].roomId;
-                    if(message_store.selected_user?.id == data[data.length-1].receiverId ){
-                        user.count = 0;
-                        message_store.setTrueReadingMessages({current_id: user_store.user?.id, room_id: message_store.selected_user_fetch_messages[0]?.roomId});
-                    }
-                    else{
-                        user.count = data[0].count;
-                    }
-                }
-            }
-            message_store.unread_messages_and_users
-        }
-    });
+// watchEffect(()=>{
+    // socket.on('broadcastunreadcountingmessages', data => {
+    //     if(data[data.length - 1].senderId == user_store.user?.id){
+    //         // Iterate All User and find it
+    //         for(let user of message_store.unread_messages_and_users){
+    //             if(user.id == data[data.length-1].receiverId ){
+    //                 // Replace finding user with sending message user
+    //                 user.roomid = data[0].roomId;
+    //                 if(message_store.selected_user?.id == data[data.length-1].receiverId ){
+    //                     user.count = 0;
+    //                     message_store.setTrueReadingMessages({current_id: user_store.user?.id, room_id: message_store.selected_user_fetch_messages[0]?.roomId});
+    //                 }
+    //                 else{
+    //                     user.count = data[0].count;
+    //                 }
+    //             }
+    //         }
+    //         message_store.unread_messages_and_users
+    //     }
+    // });
 
-})
+// })
 
 </script>
 
