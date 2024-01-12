383f50086a7ff454e52cf0197bb6f577cc8a698b
CavidanBagirliCamal <cbtechauto@gmail.com>
2024-01-12 17:20:01 +0300

    Redesign Home Page

--- src/components/home/newversioncahrt/STFSMChart.vue
+++ src/components/home/newversioncahrt/STFSMChart.vue
@@ -69,41 +69,42 @@
             </div>
         </div>
 
-        <!-- User Profile Section -->
+        <!-- Sm Section-->
         <div class="col-span-4 ml-3 bg-white rounded-xl flex flex-col items-center justify-between h-[30rem]"
             style="font-family: 'Jost';">
             <!-- Title Section -->
             <div class="w-full pl-8 border-b py-3   flex justify-between">
                 <span class="text-2xl font-bold " style="font-family: 'Roboto';">
-                    User Profile
+                    Warehouse Stock
                 </span>
                 <span class="text-2xl font-bold text-gray-500 pr-4 cursor-pointer">
                     <i class="fa-solid fa-ellipsis fa-xs"></i>
                 </span>
             </div>
             <!--  Chart Section -->
-            <div class="flex flex-col  w-full h-full items-center rounded-b-xl">
-                <div class=" w-[15rem] h-[15rem] flex justify-center items-center" style="">
-                    <img class="w-[12rem] h-[12rem] rounded-full" src="https://blog.photofeeler.com/wp-content/uploads/2017/12/linkedin-profile-picture.jpg" alt="">
+            <div class=" w-[15rem] h-[15rem] flex justify-center items-center" style="">
+                <Doughnut :data="chartDataSM" />
+                <!-- <span class="text-5xl font-bold text-violet-700">434</span> -->
+            </div>
+            <!-- Footer Section -->
+            <div class="w-full flex flex-row justify-around my-3">
+                <div class="  flex flex-col items-center">
+                    <span class="text-xl font-bold text-gray-400">Waiting</span>
+                    <span class="text-3xl font-bold">15</span>
                 </div>
-                <div class="flex flex-col justify-center items-center">
-                    <div>
-                        <span class="text-4xl font-bold text-center " >Cavidan Bagirli</span>
-                    </div>
-                    <div class="mt-2">
-                        <span class="text-xl text-center text-gray-400 " >cbagirli@ustay.com</span>
-                    </div>
-                    <div class="flex justify-between mt-5">
-                        <button class="py-2 px-3 rounded-lg mx-4 text-white font-bold bg-violet-600">Edit Profile</button>
-                        <button class="py-2 px-3 rounded-lg mx-4 text-white font-bold bg-violet-600">Edit Image</button>
-                    </div>
+                <div class="  flex flex-col items-center">
+                    <span class="text-xl font-bold text-gray-400">Complete</span>
+                    <span class="text-3xl font-bold">415</span>
+                </div>
+                <div class="  flex flex-col items-center">
+                    <span class="text-xl font-bold text-gray-400">Canceled</span>
+                    <span class="text-3xl font-bold">4</span>
                 </div>
             </div>
-           
         </div>
-
+        
         <!-- Chart Datas -->
-        <div class="col-span-6 bg-white p-2 my-3 rounded-xl flex flex-col items-center justify-around ">
+        <div class="col-span-7 bg-white p-2 my-3 rounded-xl flex flex-col items-center justify-around ">
             <div class="w-full pl-8 border-b py-3   flex justify-between">
                 <span class="text-2xl font-bold " style="font-family: 'Roboto';">
                     Group Orders
@@ -115,13 +116,134 @@
             <Bar :data="chartData" class="bg-white p-2 border-none " />
         </div>
 
+        <!-- Warehouse Datas -->
+        <div class="col-span-5  my-3 rounded-xl flex flex-col items-center justify-between "
+            style="font-family: 'Jost';">
+            <!-- <div class="w-full pl-8 border-b py-3   flex justify-between">
+                <span class="text-2xl font-bold " style="font-family: 'Roboto';">
+                    Group Orders
+                </span>
+                <span class="text-2xl font-bold text-gray-500 pr-4 cursor-pointer">
+                    <i class="fa-solid fa-ellipsis fa-xs"></i>
+                </span>
+            </div> -->
+            <div class="w-full h-full flex flex-row">
+                <div class="flex flex-col w-full h-full  ">
+                    <div class="flex flex-col h-full m-1 bg-white rounded-xl">
+                        <div class="flex flex-row justify-between items-start rounded-xl ">
+                            <div class="flex flex-col mt-4 ml-4">
+                                <span class="text-3xl" >Project</span>
+                                <span class="text-4xl font-bold mt-4" >457</span>
+                            </div>
+                            <div class="w-20 h-20 bg-blue-400 flex justify-center items-center rounded-full mr-4 mt-4">
+                                <i class="fa-solid fa-bars-progress fa-xl text-white"></i>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-full m-1 bg-white rounded-xl">
+                        <div class="flex flex-row justify-between items-start rounded-xl ">
+                            <div class="flex flex-col mt-4 ml-4">
+                                <span class="text-3xl" >Project</span>
+                                <span class="text-4xl font-bold mt-4" >457</span>
+                            </div>
+                            <div class="w-20 h-20 bg-blue-400 flex justify-center items-center rounded-full mr-4 mt-4">
+                                <i class="fa-solid fa-bars-progress fa-xl text-white"></i>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-full m-1 bg-white rounded-xl">
+                        <div class="flex flex-row justify-between items-start rounded-xl ">
+                            <div class="flex flex-col mt-4 ml-4">
+                                <span class="text-3xl" >Project</span>
+                                <span class="text-4xl font-bold mt-4" >457</span>
+                            </div>
+                            <div class="w-20 h-20 bg-blue-400 flex justify-center items-center rounded-full mr-4 mt-4">
+                                <i class="fa-solid fa-bars-progress fa-xl text-white"></i>   
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                <div class="flex flex-col w-full h-full">
+                    <div class="flex flex-col h-full m-1 bg-white rounded-xl">
+                        <div class="flex flex-row justify-between items-start rounded-xl ">
+                            <div class="flex flex-col mt-4 ml-4">
+                                <span class="text-3xl" >Project</span>
+                                <span class="text-4xl font-bold mt-4" >457</span>
+                            </div>
+                            <div class="w-20 h-20 bg-blue-400 flex justify-center items-center rounded-full mr-4 mt-4">
+                                <i class="fa-solid fa-bars-progress fa-xl text-white"></i>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-full m-1 bg-white rounded-xl">
+                        <div class="flex flex-row justify-between items-start rounded-xl ">
+                            <div class="flex flex-col mt-4 ml-4">
+                                <span class="text-3xl" >Project</span>
+                                <span class="text-4xl font-bold mt-4" >457</span>
+                            </div>
+                            <div class="w-20 h-20 bg-blue-400 flex justify-center items-center rounded-full mr-4 mt-4">
+                                <i class="fa-solid fa-bars-progress fa-xl text-white"></i>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-full m-1 bg-white rounded-xl">
+                        <div class="flex flex-row justify-between items-start rounded-xl ">
+                            <div class="flex flex-col mt-4 ml-4">
+                                <span class="text-3xl" >Project</span>
+                                <span class="text-4xl font-bold mt-4" >457</span>
+                            </div>
+                            <div class="w-20 h-20 bg-blue-400 flex justify-center items-center rounded-full mr-4 mt-4">
+                                <i class="fa-solid fa-bars-progress fa-xl text-white"></i>   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- User Profile Section -->
+        <!-- <div class="col-span-4 ml-3 bg-white rounded-xl flex flex-col items-center justify-between h-[30rem]"
+            style="font-family: 'Jost';">
+
+            <div class="w-full pl-8 border-b py-3   flex justify-between">
+                <span class="text-2xl font-bold " style="font-family: 'Roboto';">
+                    User Profile
+                </span>
+                <span class="text-2xl font-bold text-gray-500 pr-4 cursor-pointer">
+                    <i class="fa-solid fa-ellipsis fa-xs"></i>
+                </span>
+            </div>
+
+            <div class="flex flex-col  w-full h-full items-center rounded-b-xl">
+                <div class=" w-[15rem] h-[15rem] flex justify-center items-center" style="">
+                    <img class="w-[12rem] h-[12rem] rounded-full"
+                        src="https://blog.photofeeler.com/wp-content/uploads/2017/12/linkedin-profile-picture.jpg" alt="">
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <div>
+                        <span class="text-4xl font-bold text-center ">Cavidan Bagirli</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="text-xl text-center text-gray-400 ">cbagirli@ustay.com</span>
+                    </div>
+                    <div class="flex justify-between mt-5">
+                        <button class="py-2 px-3 rounded-lg mx-4 text-white font-bold bg-violet-600">Edit Profile</button>
+                        <button class="py-2 px-3 rounded-lg mx-4 text-white font-bold bg-violet-600">Edit Image</button>
+                    </div>
+                </div>
+            </div>
+
+        </div> -->
+
     </div>
 </template>
 
 <script setup>
 
 import { watchEffect, ref, onMounted } from 'vue';
-import { Bar, Pie } from 'vue-chartjs'
+import { Bar, Pie, Doughnut } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import IndexStore from '../../../store/store.index';
 import UserStore from '../../../store/store.user';
@@ -153,7 +275,7 @@ watchEffect(() => {
             backgroundColor: [
                 'rgba(61, 72, 224)',
             ],
-            
+
         }],
 
     };
--- src/components/home/newversioncahrt/TotalStatistic.vue
+++ src/components/home/newversioncahrt/TotalStatistic.vue
@@ -4,7 +4,7 @@
         <div class="col-span-2 border rounded-xl py-3 px-5 my-3 bg-white h-44 flex flex-col items-center justify-around" style="font-family: 'Figtree';">
             <div class="flex flex-row justify-between  w-full items-center">
                 <div class="flex flex-col">
-                    <span class="text-5xl font-bold " >15</span>
+                    <span class="text-4xl font-bold " >15</span>
                     <span class="text-md font-thin" style="font-family: 'Jost';">STF Waiting</span>
                 </div>
                 <img class="w-16 p-4 bg-red-200 rounded-xl" src="../../../assets/statistic_icons/waiting.png" alt="">
@@ -18,7 +18,7 @@
         <div class="col-span-2 border rounded-xl py-3 px-5 my-3 bg-white h-44 flex flex-col items-center justify-around" style="font-family: 'Figtree';">
             <div class="flex flex-row justify-between  w-full items-center">
                 <div class="flex flex-col">
-                    <span class="text-5xl font-bold " >415</span>
+                    <span class="text-4xl font-bold " >415</span>
                     <span class="text-md font-thin"  style="font-family: 'Jost';">STF Complete</span>
                 </div>
                 <img class="w-16 p-4 bg-green-200 rounded-xl" src="../../../assets/statistic_icons/complete.png" alt="">
@@ -32,7 +32,7 @@
         <div class="col-span-2 border rounded-xl py-3 px-5 my-3 bg-white h-44 flex flex-col items-center justify-around" style="font-family: 'Figtree';">
             <div class="flex flex-row justify-between  w-full items-center">
                 <div class="flex flex-col">
-                    <span class="text-5xl font-bold " >912</span>
+                    <span class="text-4xl font-bold " >912</span>
                     <span class="text-md font-thin"  style="font-family: 'Jost';">Sm Processing</span>
                 </div>
                 <img class="w-16 p-4 bg-sky-200 rounded-xl" src="../../../assets/statistic_icons/processing1.png" alt="">
@@ -46,7 +46,7 @@
         <div class="col-span-2 border rounded-xl py-3 px-5 my-3 bg-white h-44 flex flex-col items-center justify-around" style="font-family: 'Figtree';">
             <div class="flex flex-row justify-between  w-full items-center">
                 <div class="flex flex-col">
-                    <span class="text-5xl font-bold " >1446</span>
+                    <span class="text-4xl font-bold " >1446</span>
                     <span class="text-md font-thin"  style="font-family: 'Jost';">SM Complete</span>
                 </div>
                 <img class="w-16 p-4 bg-green-200 rounded-xl" src="../../../assets/statistic_icons/complete.png" alt="">
@@ -60,7 +60,7 @@
         <div class="col-span-2 border rounded-xl py-3 px-5 my-3 bg-white h-44 flex flex-col items-center justify-around" style="font-family: 'Figtree';">
             <div class="flex flex-row justify-between  w-full items-center">
                 <div class="flex flex-col">
-                    <span class="text-5xl font-bold " >511</span>
+                    <span class="text-4xl font-bold " >511</span>
                     <span class="text-md font-thin"  style="font-family: 'Jost';">Warehouse Stock</span>
                 </div>
                 <img class="w-16 p-4 bg-orange-200 rounded-xl" src="../../../assets/statistic_icons/warehouse.png" alt="">
