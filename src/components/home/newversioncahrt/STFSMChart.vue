d440cfb7510a920fb3370725ae561c64d7db2ff9
CavidanBagirliCamal <cbtechauto@gmail.com>
2024-01-12 13:54:18 +0300

    Redesign Home Page

--- src/components/home/newversioncahrt/STFSMChart.vue
+++ src/components/home/newversioncahrt/STFSMChart.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="grid grid-cols-12 gap-6 my-3">
+    <div class="grid grid-cols-12 gap-6 my-3 ">
 
-        <div class="col-span-4 bg-white rounded-xl flex flex-col items-center justify-between" style="font-family: 'Jost';">
+        <!-- STF Section-->
+        <div class="col-span-4 bg-white rounded-xl flex flex-col items-center justify-between h-[30rem]"
+            style="font-family: 'Jost';">
+            <!-- Title Section -->
             <div class="w-full pl-8 border-b py-3   flex justify-between">
-                <span class="text-2xl font-bold "  style="font-family: 'Roboto';">
+                <span class="text-2xl font-bold " style="font-family: 'Roboto';">
                     Total STF
                 </span>
                 <span class="text-2xl font-bold text-gray-500 pr-4 cursor-pointer">
                     <i class="fa-solid fa-ellipsis fa-xs"></i>
                 </span>
             </div>
-            <div class="border-[25px] border-violet-600 rounded-full flex justify-center items-center my-5"
-                style="height: calc(50% - 0.59em); width: calc(50% - 0.59em);">
+            <!-- Title Center Section -->
+            <div class="border-[20px]  w-[15rem] h-[15rem] border-violet-600 rounded-full flex justify-center items-center"
+                style="">
                 <span class="text-5xl font-bold text-violet-700">434</span>
             </div>
+            <!-- Footer Section -->
             <div class="w-full flex flex-row justify-around my-3">
                 <div class="  flex flex-col items-center">
                     <span class="text-xl font-bold text-gray-400">Waiting</span>
@@ -29,27 +34,86 @@
                 </div>
             </div>
         </div>
-        <div class="col-span-8 bg-white p-1 rounded-xl flex flex-col items-center justify-around  ">
-            <!-- <div class="w-full pl-8 border-b pb-3 pt-2  flex justify-between" style="font-family: 'Roboto';">
-                <span class="text-2xl font-bold ">
-                    Group Order 
+
+        <!-- Sm Section-->
+        <div class="col-span-4 ml-4 bg-white rounded-xl flex flex-col items-center justify-between h-[30rem]"
+            style="font-family: 'Jost';">
+            <!-- Title Section -->
+            <div class="w-full pl-8 border-b py-3   flex justify-between">
+                <span class="text-2xl font-bold " style="font-family: 'Roboto';">
+                    Total SM
                 </span>
                 <span class="text-2xl font-bold text-gray-500 pr-4 cursor-pointer">
                     <i class="fa-solid fa-ellipsis fa-xs"></i>
                 </span>
-            </div> -->
-            <Bar :data="chartData" />
+            </div>
+            <!--  Chart Section -->
+            <div class=" w-[15rem] h-[15rem] flex justify-center items-center" style="">
+                <Pie :data="chartDataSM" />
+                <!-- <span class="text-5xl font-bold text-violet-700">434</span> -->
+            </div>
+            <!-- Footer Section -->
+            <div class="w-full flex flex-row justify-around my-3">
+                <div class="  flex flex-col items-center">
+                    <span class="text-xl font-bold text-gray-400">Waiting</span>
+                    <span class="text-3xl font-bold">15</span>
+                </div>
+                <div class="  flex flex-col items-center">
+                    <span class="text-xl font-bold text-gray-400">Complete</span>
+                    <span class="text-3xl font-bold">415</span>
+                </div>
+                <div class="  flex flex-col items-center">
+                    <span class="text-xl font-bold text-gray-400">Canceled</span>
+                    <span class="text-3xl font-bold">4</span>
+                </div>
+            </div>
         </div>
 
+        <!-- User Profile Section -->
+        <div class="col-span-4 ml-4 bg-white rounded-xl flex flex-col items-center justify-between h-[30rem]"
+            style="font-family: 'Jost';">
+            <!-- Title Section -->
+            <div class="w-full pl-8 border-b py-3   flex justify-between">
+                <span class="text-2xl font-bold " style="font-family: 'Roboto';">
+                    User Profile
+                </span>
+                <span class="text-2xl font-bold text-gray-500 pr-4 cursor-pointer">
+                    <i class="fa-solid fa-ellipsis fa-xs"></i>
+                </span>
+            </div>
+            <!--  Chart Section -->
+            <div class="flex flex-col  w-full h-full items-center rounded-b-xl">
+                <div class=" w-[15rem] h-[15rem] flex justify-center items-center" style="">
+                    <img class="w-[12rem] h-[12rem] rounded-full" src="https://blog.photofeeler.com/wp-content/uploads/2017/12/linkedin-profile-picture.jpg" alt="">
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <div>
+                        <span class="text-4xl font-bold text-center " >Cavidan Bagirli</span>
+                    </div>
+                    <div>
+                        <span class="text-xl  text-center text-gray-400 " >cbagirli@ustay.com</span>
+                    </div>
+                    <div class="flex justify-between mt-5">
+                        <button class="p-2 rounded-lg mx-3 text-white font-bold bg-violet-600">Edit Profile</button>
+                        <button class="p-2 rounded-lg mx-3 text-white font-bold bg-violet-600">Edit Image</button>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
 
+        <!-- Chart Datas -->
+        <div class="col-span-6 bg-white p-2 my-3 rounded-xl flex flex-col items-center justify-around ">
+            <Bar :data="chartData" class="bg-white p-2 border-none " />
+        </div>
 
     </div>
 </template>
 
 <script setup>
 
-import { watchEffect, reactive, ref, onMounted } from 'vue';
-import { Bar } from 'vue-chartjs'
+import { watchEffect, ref, onMounted } from 'vue';
+import { Bar, Pie } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import IndexStore from '../../../store/store.index';
 import UserStore from '../../../store/store.user';
@@ -68,6 +132,7 @@ onMounted(async () => {
 // });
 
 const chartData = ref()
+const chartDataSM = ref();
 
 watchEffect(() => {
 
@@ -84,32 +149,40 @@ watchEffect(() => {
             label: 'Group Order Count',
             data: data,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)',
-                'rgba(201, 203, 107, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
+                'rgba(107, 3, 252)',
             ],
-            borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)',
-                'rgb(201, 203, 207)',
-                'rgb(201, 203, 207)',
-                'rgb(201, 203, 207)',
-            ],
-            borderWidth: 1,
+            
         }],
-        
+
     };
+
+    chartDataSM.value = {
+        // labels: ['Canceled', 'Processing', 'Completed'],
+        datasets: [
+            {
+                label: 'Data One',
+                backgroundColor: [
+                    'rgb(3, 107, 252)',
+                    'rgb(107, 3, 252)',
+                    'rgb(61, 72, 224)'
+                ],
+                data: [
+                    15, 23, 47
+                    //   index_store?.statistic_data?.sm_canceled,
+                    //   index_store?.statistic_data?.sm_process,
+                    //   index_store?.statistic_data?.sm_completed
+                ]
+            }
+        ]
+    }
+
 })
 
 
